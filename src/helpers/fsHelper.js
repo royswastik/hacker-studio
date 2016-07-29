b4fd7d091ca386cb0f7801cdd4b14da747630ebf
Swastik Roy <swastikroy1993@gmail.com>
2016-07-29 19:32:04 +0530

    Added fsHelper and page changing

--- src/helpers/fsHelper.js
+++ src/helpers/fsHelper.js
@@ -1,50 +1,91 @@
+let win;
 module.exports = {
+    window: {
+        "loading": false,
+        "win": null,
+        "open": false,
+        "parentWindowId": null
+    },
+    open: function(){
+        if(this.window.open == false){
+            const windowID = BrowserWindow.getFocusedWindow().id;
+            this.window.parentWindowId = windowID;
+             // const invisPath = 'file://' + path.join(__dirname, './background/fsHelper.html');
+            let invisPath = 'file:///home/swastik/dev/hs/hacker_studio/src/background/fsHelper.html';
+            this.window.win = new BrowserWindow({ width: 400, height: 400, show: false });
+            this.window.win.loadURL(invisPath);
+            this.window.loading = true;
+            let winTemp = this.window;
+            this.window.win.webContents.on('did-finish-load', function () {
+                winTemp.loading = false;
+                winTemp.open = true;
+                const input = 100;
+                console.log("Fs Helper Created");
+            });
+        }else{
+            console.log("Window is already open");
+        }
+        
+    },
+    close: function(){
+        if(this.window.open == true){
+            const windowID = this.window.parentWindowId;
+            this.window.win.webContents.send("close-fs-helper", windowID);
+            this.window.open = false;
+            this.window.win = null;
+            console.log(windowID);
+        }
+        else{
+            console.log("Window is already closed");
+        }
+        
+    },
     getProblemsByCategoryId: function (categoryId) {
-        const windowID = BrowserWindow.getFocusedWindow().id;
-        const invisPath = 'file://' + path.join(__dirname, './background/fsHelper.html');
-        let win = new BrowserWindow({ width: 400, height: 400, show: false });
-        win.loadURL(invisPath);
-
-        win.webContents.on('did-finish-load', function () {
+        const windowID = this.window.parentWindowId;
+        if(this.window.open == true){
+            this.window.win.webContents.send('get-problems-by-categoryId-fs', categoryId, windowID);
+            return;
+        }
+        let winTemp = this.window.win;
+        this.window.win.webContents.on('did-finish-load', function () {
             const input = 100;
-            win.webContents.send('get-problem-by-categoryId-fs', categoryId, windowID);
+            winTemp.webContents.send('get-problems-by-categoryId-fs', categoryId, windowID);
         });
     },
     getAllCategories: function () {
-        const windowID = BrowserWindow.getFocusedWindow().id;
-        let invisPath = 'file:///D:/workspace/desktop/hacker-studio/src/background/fsHelper.html';
-        console.log(invisPath);
-        invisPath2 = invisPath.replace(new RegExp("\\".replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), 'g'), "/");
-        console.log(invisPath);
-        let win = new BrowserWindow({ width: 400, height: 400, show: false });
-        win.loadURL(invisPath2);
-        console.log("getAllCategories");
-        win.webContents.on('did-finish-load', function () {
-            console.log("DID finish load");
+        const windowID = this.window.parentWindowId;
+        if(this.window.open == true){
+            this.window.win.webContents.send('get-categories-fs', input, windowID);
+            return;
+        }
+        let winTemp = this.window.win;
+        this.window.win.webContents.on('did-finish-load', function () {
             const input = 100;
-            win.webContents.send('get-categories-fs', input, windowID);
+            winTemp.webContents.send('get-categories-fs', input, windowID);
         });
     },
     getAllProblems : function (skip, limit) {
-        const windowID = BrowserWindow.getFocusedWindow().id;
-        const invisPath = 'file://' + path.join(__dirname, './background/fsHelper.html');
-        let win = new BrowserWindow({ width: 400, height: 400, show: false });
-        win.loadURL(invisPath);
-
-        win.webContents.on('did-finish-load', function () {
+        const windowID = this.window.parentWindowId;
+        if(this.window.open == true){
+            this.window.win.webContents.send('get-all-problems-fs', input, windowID);
+            return;
+        }
+        let winTemp = this.window.win;
+        this.window.win.webContents.on('did-finish-load', function () {
             const input = 100;
-            win.webContents.send('get-all-problems-fs', input, windowID);
+            winTemp.webContents.send('get-all-problems-fs', input, windowID);
         });
     },
-    getProblemById: function (id) {
-        const windowID = BrowserWindow.getFocusedWindow().id;
-        const invisPath = 'file://' + path.join(__dirname, './background/fsHelper.html');
-        let win = new BrowserWindow({ width: 400, height: 400, show: false });
-        win.loadURL(invisPath);
-
-        win.webContents.on('did-finish-load', function () {
+    getProblemById: function (problemId, categoryId) {
+        const windowID = this.window.parentWindowId;
+        if(this.window.open == true){
+            this.window.win.webContents.send('get-problem-by-id-fs', problemId, categoryId, windowID);
+            return;
+        }
+        let winTemp = this.window.win;
+        this.window.win.webContents.on('did-finish-load', function () {
             const input = 100;
-            win.webContents.send('get-problem-by-id-fs', problemId, windowID);
+            winTemp.webContents.send('get-problem-by-id-fs', problemId, categoryId, windowID);
         });
     },
     searchProblemsByName: function (text) {
--- src/helpers/pager.js
+++ src/helpers/pager.js
@@ -1,7 +1,18 @@
 let jQuery = require("../assets/js/jquery.min.js");
 
 module.exports = {
-    loadPage : function (pageName,$elem) {
-        $elem.load("./pages/"+pageName+".html")
+    loadPage : function (pageName,$elem, options) {
+        $elem.load("./pages/"+pageName+".html");
+        if(pageName == "problem"){
+            if(!(options["problemId"]) || !(options["categoryId"])){
+                console.log("Please provide both problemId and categoryId");
+            }
+            let problemId = options["problemId"];
+            let categoryId = options["categoryId"];
+            fsHelper.open();
+            console.log(problemId);
+            fsHelper.getProblemById(problemId, categoryId);
+            fsHelper.close();
+        }
     }
 }
