00f24a239de71ddce2547fa4dd3dc1c6adb211a6
Swastik Roy <swastikroy1993@gmail.com>
2016-07-29 19:40:18 +0530

    Changed html blocks

--- src/assets/js/append-blocks.js
+++ src/assets/js/append-blocks.js
@@ -5,7 +5,7 @@ function getQuestionItem(name, id){
                             <div class="panel-body">\
                                 <div class="row">\
                                     <div class="col-xs-9">\
-                                        <h4 class="list-group-item-heading" style="color:#2ec866;">'+name+'</h4>\
+                                        <h4 data-problem-id="'+id+'" class="list-group-item-heading solve-btn" style="color:#2ec866;cursor:pointer;">'+name+'</h4>\
                                     </div>\
                                     <div class="col-xs-3">\
                                         <h4 class="list-group-item-heading" style="text-align:right; padding-right:20px;">A B C</h4>\
@@ -17,7 +17,7 @@ function getQuestionItem(name, id){
                                         <p>Success Rate: 98.10% Max Score: 1 Difficulty: Easy</p>\
                                     </div>\
                                     <div class="col-xs-3">\
-                                        <a href="question/id" class="btn btn-default pull-right" style="margin-right:20px;color: #6e6e6e;font-size: 12pt;font-weight: 400;" type="submit">Solve</a>\
+                                        <button data-problem-id="'+id+'" class="solve-btn btn btn-default pull-right" style="margin-right:20px;color: #6e6e6e;font-size: 12pt;font-weight: 400;" type="submit">Solve</button>\
                                     </div>\
                                     \
                                 </div>\
